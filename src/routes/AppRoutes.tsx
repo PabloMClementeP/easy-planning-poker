9b457c7485b89b680225daa2ca9d3200a8b330ff
Pablo Clemente <pclementep@gmail.com>
2025-05-08 20:11:06 -0300

    auth callback created to prevent sending more than one email

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Dashboard from "../pages/dashboard";
 import Login from "../pages/login";
 import { Session } from "@supabase/supabase-js";
 import Room from "../pages/room";
+import AuthCallback from "../pages/auth-callback";
 
 type Props = {
   session: Session | undefined;
@@ -20,6 +21,7 @@ const AppRoutes = ({ session }: Props) => {
         path="/rooms/:id"
         element={session ? <Room session={session} /> : <Navigate to="/" />}
       />
+      <Route path="/auth/callback" element={<AuthCallback />} />
     </Routes>
   );
 };
